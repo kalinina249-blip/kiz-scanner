86b5ad52cc3eff0dfa7234469eece7603024d04c
kalinina249-blip <kalinina249@gmail.com>
2026-07-14 20:40:49 +0300

    upload start-cz-helper.bat

--- start-cz-helper.bat
+++ start-cz-helper.bat
@@ -1,7 +1,7 @@
 @echo off
 title CHZ helper - ne zakryvat
 echo Skachivayu svezhuyu versiyu pomoshchnika...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol=3072; try { Invoke-WebRequest 'https://kalinina249-blip.github.io/kiz-scanner/cz-helper.ps1' -OutFile \"$env:TEMP\cz-helper.ps1\" -UseBasicParsing -TimeoutSec 30 } catch { Write-Host 'Ne udalos skachat (net interneta?)' -ForegroundColor Red }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol=3072; try { Invoke-WebRequest 'https://kalinina249-blip.github.io/kiz-scanner/cz-helper.ps1?v=%RANDOM%' -OutFile \"$env:TEMP\cz-helper.ps1\" -UseBasicParsing -TimeoutSec 30 } catch { Write-Host 'Ne udalos skachat (net interneta?)' -ForegroundColor Red }"
 if exist "%TEMP%\cz-helper.ps1" (
   powershell -NoProfile -ExecutionPolicy Bypass -File "%TEMP%\cz-helper.ps1"
 ) else if exist "%~dp0cz-helper.ps1" (
