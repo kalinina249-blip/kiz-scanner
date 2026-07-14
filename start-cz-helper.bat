dd3a1131233a574a1bfa336b883b042013110330
kalinina249-blip <kalinina249@gmail.com>
2026-07-14 11:35:44 +0300

    upload start-cz-helper.bat

--- start-cz-helper.bat
+++ start-cz-helper.bat
@@ -1,4 +1,13 @@
 @echo off
 title CHZ helper - ne zakryvat
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0cz-helper.ps1"
+echo Skachivayu svezhuyu versiyu pomoshchnika...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol=3072; try { Invoke-WebRequest 'https://kalinina249-blip.github.io/kiz-scanner/cz-helper.ps1' -OutFile \"$env:TEMP\cz-helper.ps1\" -UseBasicParsing -TimeoutSec 30 } catch { Write-Host 'Ne udalos skachat (net interneta?)' -ForegroundColor Red }"
+if exist "%TEMP%\cz-helper.ps1" (
+  powershell -NoProfile -ExecutionPolicy Bypass -File "%TEMP%\cz-helper.ps1"
+) else if exist "%~dp0cz-helper.ps1" (
+  powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0cz-helper.ps1"
+) else (
+  echo.
+  echo OSHIBKA: ne udalos poluchit cz-helper.ps1. Proverte internet i zapustite snova.
+)
 pause
